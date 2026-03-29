19f7cb9df5ea5d0911c072299f43e134654090ec
mantoniogamaa <gamaafer@gmail.com>
2026-03-29 19:32:10 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.FLUXO_CONFIG = {
   useSupabase: true,
   supabaseUrl: 'https://fbfjczmajurunybofhqb.supabase.co',
-  supabaseAnonKey: 'sb_publishable_p2PvcbXSy8gI0qzMbXOnEA_nzP0BOYA',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZiZmpjem1hanVydW55Ym9maHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDgxMjIsImV4cCI6MjA5MDM4NDEyMn0.vNrOQ8khVf43_e33-Dz1SHoiyAh31X3Xbuf7guiXFeQ',
   workspaceId: 'principal',
   appName: 'Fluxo',
   persistMode: 'hybrid', // 'hybrid' = local + supabase
